8d8b04a035910130dbd343c760ddfa951be1c3bd
Unknown <g1727009@st.t-kougei.ac.jp>
2018-06-18 18:19:10 +0900

    最初のコミット

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;　// LoadSceneを使うために必要!!
 
 public class PlayerController : MonoBehaviour {
+
     Rigidbody2D rigid2D;
     Animator animator;
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSeed = 2.0f;
+
 	void Start () {
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
 	}
 
-
-    void Update() {
+    void Update()
+    {
         // ジャンプする
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            this.rigid2D.velocity.y == 0)
+        {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -29,16 +34,29 @@ public class PlayerController : MonoBehaviour {
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
         // スピード制限
-        if (speedx < this.maxWalkSeed) { 
+        if (speedx < this.maxWalkSeed)
+        {
             this.rigid2D.AddForce(transform.right * key * this.walkForce);
         }
 
         // 反転対策
-        if (key != 0) {
+        if (key != 0)
+        {
             transform.localScale = new Vector3(key, 1, 1);
         }
 
         // プレイヤの速度に応じてアニメーション速度を変える
         this.animator.speed = speedx / 2.0f;
+        // 画面外に出た場合は最初から
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+    // ゴールに到達
+    void OnTriggerEnter2D(Collider2D collision) {
+    Debug.Log("ゴール");
+    SceneManager.LoadScene("ClearScene");
     }
 }
